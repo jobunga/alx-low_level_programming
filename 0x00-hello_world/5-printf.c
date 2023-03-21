8dd4c42cf8eb71f011e9ef54cadaf47a1768f35f
jobunga <jeankth5@gmail.com>
2023-03-21 13:06:50 -0700

    hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
-#include <stdo.h>
+#include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
         puts("with proper grammar, but the outcome is a piece of art,\n");
